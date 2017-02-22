c11861aa642228623b6dbe4366dbc646a7b7e87f
Thomas-Rudge <thomas.rudge85@gmail.com>
2017-02-22 21:59:55 +0000

    Static pages
    Added static pages

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
 
   get    '/signup',    to: 'users#new'
   post   '/signup',    to: 'users#create'
+
+  # Static pages
+  get '/legal', to: 'static_pages#legal'
+  get '/about', to: 'static_pages#about'
+  get '/help',  to: 'static_pages#help'
 end
