857c09f0f3ea46a44aa458ee57fe28c4f9a4883c
Thomas-Rudge <thomas.rudge85@gmail.com>
2017-03-08 18:21:25 +0000

    Login now keeps user on current page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     if @user && @user.authenticate(params[:session][:password])
       log_in @user
       params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
-      redirect_to root_url
+      redirect_to :back
     else
       flash.now[:danger] = "Unknown email/password combination."
       render 'new'
