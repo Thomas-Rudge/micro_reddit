a1472892f3090d70c79e04437346ed4e5b922275
Thomas-Rudge <thomas.rudge85@gmail.com>
2017-03-12 20:37:51 +0000

    Further styling

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170310180922) do
+ActiveRecord::Schema.define(version: 20170312194142) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "content"
@@ -31,11 +31,12 @@ ActiveRecord::Schema.define(version: 20170310180922) do
     t.integer  "post_type"
     t.integer  "user_id"
     t.integer  "subreddit_id"
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
-    t.integer  "upvotes",      default: 0
-    t.integer  "downvotes",    default: 0
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
+    t.integer  "upvotes",       default: 0
+    t.integer  "downvotes",     default: 0
     t.string   "thumbnail"
+    t.integer  "comment_count", default: 0
     t.index ["subreddit_id"], name: "index_posts_on_subreddit_id"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
