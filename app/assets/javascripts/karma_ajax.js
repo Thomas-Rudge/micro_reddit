ac42f1b1bde4f6da0f66c828c06a82099ddf79aa
Thomas-Rudge <thomas.rudge85@gmail.com>
2017-03-15 08:45:01 +0000

    Created karma controller
    Karma controller manages the karma of users, posts, and comments

--- app/assets/javascripts/karma_ajax.js
+++ app/assets/javascripts/karma_ajax.js
@@ -1,30 +1,75 @@
 $(document).ready(function() {
-  var $postThumb = $('#post_thumbnail');
-  var $postTitle = $('#post_title')
 
-  var updateKarma = function(data,
+  var updateKarma = function(objectDetails, karma) {
+    var type = {"c": "comment", "p": "post"}[objectDetails[1]];
+    var id = objectDetails.slice(2);
+    var postId = $('#'+objectDetails).attr('data');
 
-    $.get('/post_title', {url : link}, function(data, status) {
-      if (status == 'success') {
-        if (data.title != '') {
-          $dynamicTitle.slideDown();
-          $suggestedTitle.text(data.title);
-          $postThumb.val(data.thumbnail);
-          $postTextBox.slideUp();
-        } else {
-          $suggestedTitle.text("");
-          $postThumb.val("");
-        };
+    $.post('/karma', {id: id, type: type, karma: karma, postid: postId}, function(data, status) {
+      console.log('Karma>'+karma);
+      if (status != 'success') {
+        return null;
       };
-  });
 
-  $('#use_suggested_title').on('click', function(e) {
+      var $upvoteSpan = $('#u'+objectDetails.slice(1));
+      var $downvoteSpan = $('#d'+objectDetails.slice(1));
+      var $karmaSpan = $('#k'+objectDetails.slice(1));
+      var points = parseInt($karmaSpan.text(), 10);
+      // If user clicks upvote with downvote highlighted, or
+      // visa-versa, the vote count should change by 2.
+      if ([1, -1].includes(karma)) {
+        if (karma == 1 && $downvoteSpan.hasClass('downvoted')){
+          points += 2;
+        } else if (karma == -1 && $upvoteSpan.hasClass('upvoted')){
+          points -= 2;
+        } else {
+          points += karma;
+        };
+      } else if (objectDetails[0] == 'u') {
+        --points;
+      } else {
+        ++points;
+      };
+      //
+      switch(karma) {
+        case 0: //Remove all highlighting
+          $upvoteSpan.removeClass('upvoted');
+          $downvoteSpan.removeClass('downvoted');
+          $karmaSpan.removeClass('upvoted downvoted');
+          break;
+        case 1: //Highlight upvote
+          $upvoteSpan.addClass('upvoted');
+          $karmaSpan.removeClass('downvoted');
+          $karmaSpan.addClass('upvoted');
+          $downvoteSpan.removeClass('downvoted');
+          break;
+        case -1: //Highlight downvote
+          $downvoteSpan.addClass('downvoted');
+          $karmaSpan.removeClass('upvoted');
+          $karmaSpan.addClass('downvoted');
+          $upvoteSpan.removeClass('upvoted');
+          break;
+      };
+      //Update points
+      $karmaSpan.text(points.toString());
+    });
+  };
+
+  $('.upvote').on('click', function(e) {
     e.preventDefault();
-    var title = $suggestedTitle.text();
-    console.log('Title: ' + title)
-    $postTitle.val(title);
+    if ($(e.target).hasClass('upvoted')) {
+      updateKarma(this.id, 0);
+    } else {
+      updateKarma(this.id, 1);
+    };
   });
 
-  $('.upvote').on('click', updateKarma(this.data, 1));
-  $('.downvote').on('click', updateKarma(this.data, 0));
+  $('.downvote').on('click', function(e) {
+    e.preventDefault();
+    if ($(e.target).hasClass('downvoted')) {
+      updateKarma(this.id, 0);
+    } else {
+      updateKarma(this.id, -1);
+    };
+  });
 });
--- app/controllers/karma_controller.rb
+++ app/controllers/karma_controller.rb
@@ -1,4 +1,93 @@
 class KarmaController < ApplicationController
+  before_action :check_login_status
+
   def update
+    @ok_params = params.permit(:id, :type, :karma, :postid)
+
+    head 403 if bad_params?
+
+    case @ok_params[:karma].to_i
+    when 1  then upvote
+    when 0  then remove_vote
+    when -1 then downvote
+    end
+
+    head 202
   end
+
+  def check_login_status
+    head 403 unless logged_in?
+  end
+
+  private
+
+    def upvote
+      if @ok_params[:type] == "post"
+        vote = Vote.new(user_id: current_user.id, post_id: @ok_params[:id].to_i, vote: 1)
+        vote_target = Post.find(@ok_params[:id].to_i)
+      else
+        vote = Vote.new(user_id: current_user.id, post_id: @ok_params[:postid].to_i,
+                        comment_id: @ok_params[:id].to_i, vote: 1)
+        vote_target = Comment.find(@ok_params[:id].to_i)
+      end
+
+      unless vote_target.nil?
+        vote_target.increment(:upvotes)
+        vote_target.save rescue nil
+        vote.save        rescue nil
+      end
+    end
+
+    def downvote
+      if params[:type] == "post"
+        vote = Vote.new(user_id: current_user.id, post_id: @ok_params[:id].to_i, vote: 0)
+        vote_target = Post.find(@ok_params[:id].to_i)
+      else
+        vote = Vote.new(user_id: current_user.id, post_id: @ok_params[:postid].to_i,
+                        comment_id: @ok_params[:id].to_i, vote: 0)
+        vote_target = Comment.find(@ok_params[:id].to_i)
+      end
+
+      unless vote_target.nil? || vote.nil?
+        vote_target.increment(:downvotes)
+        vote_target.save rescue nil
+        vote.save        rescue nil
+      end
+    end
+
+    def remove_vote
+      if params[:type] == "post"
+        vote = Vote.find_by(user_id: current_user.id, post_id: @ok_params[:id].to_i, comment_id: nil)
+        vote_target = Post.find(@ok_params[:id].to_i)
+      else
+        vote = Vote.find_by(user_id: current_user.id, post_id: @ok_params[:postid].to_i,
+                            comment_id: @ok_params[:id].to_i)
+        vote_target = Comment.find(@ok_params[:id].to_i)
+      end
+
+      unless vote_target.nil? || vote.nil?
+        case vote.vote
+        when 0 then vote_target.decrement(:downvotes)
+        when 1 then vote_target.decrement(:upvotes)
+        end
+
+        vote_target.save
+      end
+
+      vote.destroy rescue nil
+    end
+
+    def bad_params?
+      if @ok_params[:id].to_i.to_s != @ok_params[:id]
+        false
+      elsif @ok_params[:postid].to_i.to_s != @ok_params[:postid]
+        false
+      elsif ![-1, 0, 1].include? @ok_params[:karma]
+        false
+      elsif !["comment", "post"].include? @ok_params[:type]
+        false
+      else
+        true
+      end
+    end
 end
