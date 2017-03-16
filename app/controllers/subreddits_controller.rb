9bfc65fd7b3f5d1072837733b7a914294e00e924
Thomas-Rudge <thomas.rudge85@gmail.com>
2017-03-16 14:41:23 +0000

    Added subreddit index and show pages
    All subreddits can now be viewed from index page, and each subreddit and its posts can be viewed from show. Both actions check user voted before rendering pages.

--- app/controllers/subreddits_controller.rb
+++ app/controllers/subreddits_controller.rb
@@ -1,6 +1,9 @@
 class SubredditsController < ApplicationController
 
   def index
+    @subreddits = Subreddit.order("subscriptions_count DESC").all.
+                            paginate(page: params[:page], per_page: 30)
+    @subscriptions = Subscription.where(user_id: current_user.id).pluck(:subreddit_id) rescue nil
   end
 
   def new
@@ -25,6 +28,19 @@ class SubredditsController < ApplicationController
     @subreddit = Subreddit.find_by(name: params[:name])
     if @subreddit.nil?
       flash.now[:information] = "r/#{params[:name]} doesn't exist.\nWhy don't you create it?"
+      @posts = []
+    else
+      @posts = @subreddit.posts.where("upvotes - downvotes > -10").
+                                paginate(page: params[:page], per_page: 30)
+
+      @votes = Hash.new
+      if logged_in?
+        user_votes = Vote.where(user_id: current_user.id, comment_id: nil)
+
+        user_votes.each do |vote|
+          @votes[vote.post_id] = vote.vote
+        end
+      end
     end
   end
 
