39c9f576d8d5280936ce596276bacb0032db7037
Thomas-Rudge <thomas.rudge85@gmail.com>
2017-03-22 17:35:18 +0000

    Fixed post_text link
    Before the links for post_texts were nil, but they should be a link to the posts show page.

--- app/models/post.rb
+++ app/models/post.rb
@@ -12,8 +12,7 @@ class Post < ApplicationRecord
   validates :title, presence: true,
                     length: { maximum: 255 }
 
-  validates :link, format: { with: URI::regexp(["http", "https"]) },
-                   presence: true, if: "post_type == 1"
+  validates :link, presence: true
 
   validates :post_type, presence: true,
                         inclusion: { in: [ 0, 1 ] }
@@ -26,7 +25,11 @@ class Post < ApplicationRecord
 
     def process_link
       return link if link.blank?
-      self.link = add_scheme_to_link(URI.parse(self.link)).to_s
+      if self.post_type == 0
+        self.link = "/r/#{self.subreddit.name}/#{self.id}"
+      else
+        self.link = add_scheme_to_link(URI.parse(self.link)).to_s
+      end
     end
 
     def clean_up_post_for_database
@@ -34,7 +37,7 @@ class Post < ApplicationRecord
 
       self.title = self.title.gsub(bad_chars_regex, " ")
       self.link  = self.link.gsub(bad_chars_regex, " ") unless self.link.nil?
-      self.post_type == 0 ? self.link = nil : self.post_text = nil
+      self.post_text = nil if self.post_type == 1
     end
 
     def process_thumbnail
